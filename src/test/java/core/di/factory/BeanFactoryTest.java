123174c4a4c47fbcf35482702ff8460a33543d11
joshua(박근희) <joshua.park321@kakaocorp.com>
2020-12-15 11:37:31 +0900

    요구사항6 - component scan

--- src/test/java/core/di/factory/BeanFactoryTest.java
+++ src/test/java/core/di/factory/BeanFactoryTest.java
@@ -6,9 +6,14 @@ import core.annotation.Service;
 import core.annotation.web.Controller;
 import core.di.factory.example.MyQnaService;
 import core.di.factory.example.QnaController;
+import next.reflection.ReflectionTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
 import java.util.Set;
@@ -16,6 +21,8 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BeanFactoryTest {
+    private static final Logger logger = LoggerFactory.getLogger(BeanFactoryTest.class);
+
     private Reflections reflections;
     private BeanFactory beanFactory;
 
@@ -47,5 +54,28 @@ public class BeanFactoryTest {
             beans.addAll(reflections.getTypesAnnotatedWith(annotation));
         }
         return beans;
+    }
+
+    @Test
+    public void printAnnotatedClasses() {
+        Reflections reflections = new Reflections(
+                "core.di.factory.example",
+                new TypeAnnotationsScanner(),
+                new SubTypesScanner());
+
+        logger.debug("controllers ------");
+        reflections.getTypesAnnotatedWith(Controller.class)
+                .stream()
+                .forEach(clazz -> logger.debug("{}", clazz.getName()));
+
+        logger.debug("services ------");
+        reflections.getTypesAnnotatedWith(Service.class)
+                .stream()
+                .forEach(clazz -> logger.debug("{}", clazz.getName()));
+
+        logger.debug("repositories ------");
+        reflections.getTypesAnnotatedWith(Repository.class)
+                .stream()
+                .forEach(clazz -> logger.debug("{}", clazz.getName()));
     }
 }
