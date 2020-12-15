43a71b972cef1344d98c420d5db645ee08d49663
joshua(박근희) <joshua.park321@kakaocorp.com>
2020-12-16 00:22:22 +0900

    피드백: Junit3Test 중복코드 제거

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -8,16 +8,16 @@ public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test instance = clazz.newInstance();
 
         Stream.of(clazz.getDeclaredMethods())
                 .filter(method -> method.getName().startsWith("test"))
                 .forEach(method -> {
                     try {
-                        method.invoke(clazz.newInstance());
+                        method.invoke(instance);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 });
-
     }
 }
