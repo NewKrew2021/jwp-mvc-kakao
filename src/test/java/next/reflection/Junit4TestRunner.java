ddcfaa9476c27a80fc90df9d5c91dcbe5a1658a4
nio.d <nio.d@kakaocorp.com>
2020-12-15 11:17:43 +0900

    feat: @Test 애노테이션 메소드 실행

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,37 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+
+        List<Method> annotatedMethods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> Arrays.stream(method.getAnnotations())
+                        .anyMatch(annotation -> annotation.annotationType().getSimpleName().equals("MyTest")))
+                .collect(Collectors.toList());
+
+        annotatedMethods.stream().forEach( method -> {
+            try {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        });
+
+
     }
 }
