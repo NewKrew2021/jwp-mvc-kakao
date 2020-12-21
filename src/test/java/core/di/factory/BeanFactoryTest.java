b47facc66a472ff694f9cc5c92bfdac9bf11b851
seihwan choi <sehan.choi@gmail.com>
2020-12-21 18:36:21 +0900

    Step1 자바 reflection (#2)
    * feat: 클래스 정보출력
    
    * feat: test 로 시작하는 메소드 실행
    
    * feat: @Test 애노테이션 메소드 실행
    
    * refactor: isAnnotationPresent 사용해서 @MyTest 존재여부체크
    
    * feat: private field에 값 할당
    
    * feat: 인자를 가진 생성자의 인스턴스 생성
    
    * feat: component scan
    
    * chore: 불필요한 코드삭제
    
    * refactor: pr 피드백 반영
    
    > java.lang.reflect.Modifier 사용하기
    > 생성자 시그니쳐 출력포멧 다듬기
    > constructor 호출 검증테스트 개선
    
    * test: BeanFactory#di test 추가
    
    Co-authored-by: nio.d <nio.d@kakaocorp.com>

--- src/test/java/core/di/factory/BeanFactoryTest.java
+++ src/test/java/core/di/factory/BeanFactoryTest.java
@@ -4,18 +4,22 @@ import com.google.common.collect.Sets;
 import core.annotation.Repository;
 import core.annotation.Service;
 import core.annotation.web.Controller;
-import core.di.factory.example.MyQnaService;
-import core.di.factory.example.QnaController;
+import core.di.factory.example.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BeanFactoryTest {
+    private Logger logger = LoggerFactory.getLogger(BeanFactoryTest.class);
+
     private Reflections reflections;
     private BeanFactory beanFactory;
 
@@ -29,15 +33,50 @@ public class BeanFactoryTest {
     }
 
     @Test
+    void scanController(){
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        assertThat(controllers).containsExactly(QnaController.class);
+        controllers.stream()
+                .forEach(controller -> {
+                    logger.debug(controller.getName());
+                });
+    }
+
+    @Test
+    void scanService(){
+        Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        assertThat(services).containsExactly(MyQnaService.class);
+        services.stream()
+                .forEach(service -> {
+                    logger.debug(service.getName());
+                });
+    }
+
+    @Test
+    void scanRepository(){
+        Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+        assertThat(repositories).containsExactlyInAnyOrder(JdbcUserRepository.class, JdbcQuestionRepository.class);
+        repositories.stream()
+                .forEach(repository -> {
+                    logger.debug(repository.getName());
+                });
+    }
+
+    @Test
     public void di() throws Exception {
         QnaController qnaController = beanFactory.getBean(QnaController.class);
 
         assertNotNull(qnaController);
         assertNotNull(qnaController.getQnaService());
+        assertNotNull(qnaController.getQnaService().getUserRepository());
+        assertNotNull(qnaController.getQnaService().getQuestionRepository());
 
-        MyQnaService qnaService = qnaController.getQnaService();
-        assertNotNull(qnaService.getUserRepository());
-        assertNotNull(qnaService.getQuestionRepository());
+        MyQnaService qnaService = beanFactory.getBean(MyQnaService.class);
+        assertThat(qnaController.getQnaService() == qnaService).isTrue();
+
+        UserRepository userRepository = beanFactory.getBean(JdbcUserRepository.class);
+        assertThat(userRepository == qnaController.getQnaService().getUserRepository()).isTrue();
+        assertThat(userRepository == qnaService.getUserRepository()).isTrue();
     }
 
     @SuppressWarnings("unchecked")
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -12,7 +21,38 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
+
         logger.debug(clazz.getName());
+
+        // 생성자
+        Arrays.stream(clazz.getDeclaredConstructors())
+                .forEach(constructor -> {
+                    logger.debug(MessageFormat.format("constructors: {0} {1} ({2})",
+                            Modifier.toString(constructor.getModifiers()),
+                            clazz.getName(),
+                            Arrays.stream(constructor.getParameterTypes()).map(it -> it.getName()).collect(Collectors.joining(", "))));
+                });
+
+        // 필드
+        Arrays.stream(clazz.getDeclaredFields())
+                .forEach(field -> {
+                    logger.debug(MessageFormat.format("fields: {0} {1}(type:{2})",Modifier.toString(field.getModifiers()), field.getName(), field.getType() ));
+                });
+
+        // 매소드
+        Arrays.stream(clazz.getDeclaredMethods())
+                .forEach(method -> {
+                    logger.debug(MessageFormat.format("methods: {0} {1} {2} ({3})",
+                            Modifier.toString(method.getModifiers()),
+                            method.getReturnType(),
+                            method.getName(),
+                            Arrays.stream(method.getParameters())
+                                    .map(it -> it.getType().getName())
+                                    .collect(Collectors.joining(" ,"))
+                    ));
+
+                });
+
     }
 
     @Test
@@ -28,4 +68,51 @@ public class ReflectionTest {
             }
         }
     }
+
+    @Test
+    public void privateFieldAccess() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
+        Class<Student> clazz = Student.class;
+
+        Student student = clazz.getDeclaredConstructor().newInstance();
+
+        Field name = clazz.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(student, "sehan");
+        Field age = clazz.getDeclaredField("age");
+        age.setAccessible(true);
+        age.set(student, 18);
+
+        assertThat(student.getName()).isEqualTo("sehan");
+        assertThat(student.getAge()).isEqualTo(18);
+
+    }
+
+    @Test
+    void createInstanceByConstructor() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class clazz = Question.class;
+
+        Constructor<Question> constructor1 = clazz.getDeclaredConstructor(String.class, String.class, String.class);
+        Question q1 = constructor1.newInstance("nio", "title", "contents");
+
+        assertThat(q1)
+                .hasFieldOrPropertyWithValue("writer", "nio")
+                .hasFieldOrPropertyWithValue("title", "title")
+                .hasFieldOrPropertyWithValue("contents", "contents");
+
+        Date now = new Date();
+        Constructor<Question> constructor2 = clazz.getDeclaredConstructor(long.class, String.class, String.class, String.class, Date.class, int.class);
+        Question q2 = constructor2.newInstance(1L, "nio", "title", "contents", now, 0);
+
+        assertThat(q2)
+                .hasFieldOrPropertyWithValue("questionId", 1L)
+                .hasFieldOrPropertyWithValue("writer", "nio")
+                .hasFieldOrPropertyWithValue("title", "title")
+                .hasFieldOrPropertyWithValue("contents", "contents")
+                .hasFieldOrPropertyWithValue("createdDate", now)
+                .hasFieldOrPropertyWithValue("countOfComment", 0);
+
+
+    }
+
+
 }
