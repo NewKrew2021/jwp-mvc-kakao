b4327909b1cf01cb4f55027312eff677527ba1f7
genos.lee <genos.lee@kakaocorp.com>
2021-01-04 18:20:30 +0900

    test: 요구사항1 - 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +18,41 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        printFields(clazz);
+        printConstructors(clazz);
+        printMethods(clazz);
+    }
+
+    private void printFields(Class<Question> clazz) {
+        System.out.println("[FIELDS]");
+        for (Field field : clazz.getDeclaredFields()) {
+            System.out.printf("field: %s %s%n", field.getType().getSimpleName(), field.getName());
+        }
+        System.out.println();
+    }
+
+    private void printConstructors(Class<Question> clazz) {
+        System.out.println("[CONSTRUCTORS]");
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            System.out.printf("constructor: %s (%s)%n", constructor.getName(), toString(constructor.getParameters()));
+        }
+        System.out.println();
+    }
+
+    private String toString(Parameter[] parameters) {
+        return Arrays.stream(parameters)
+                .map(Parameter::getType)
+                .map(Class::getSimpleName)
+                .collect(Collectors.joining(","));
+    }
+
+    private void printMethods(Class<Question> clazz) {
+        System.out.println("[METHODS]");
+        for (Method method : clazz.getDeclaredMethods()) {
+            System.out.printf("method: %s %s(%s)%n", method.getReturnType().getSimpleName(), method.getName(), toString(method.getParameters()));
+        }
+        System.out.println();
     }
 
     @Test
