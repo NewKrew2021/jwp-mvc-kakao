b64082dabefb446d2f5ff66a6c630f3136413b1c
genos.lee <genos.lee@kakaocorp.com>
2021-01-06 13:52:17 +0900

    feature: 요구사항 1 - 애노테이션 기반 MVC 프레임워크

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,21 @@ import core.mvc.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
+    private final Method method;
+    private final Object controllerObject;
+
+    public HandlerExecution(Method method, Object controllerObject) {
+        this.method = method;
+        this.controllerObject = controllerObject;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (method != null && controllerObject != null) {
+            return (ModelAndView) method.invoke(controllerObject, request, response);
+        }
         return null;
     }
 }
