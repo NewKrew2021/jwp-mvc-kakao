fed73b3018db2b230c0aa2ff0381b7c80a54a2fe
genos.lee <genos.lee@kakaocorp.com>
2021-01-07 11:52:54 +0900

    refactor: /users/profile 접근을 애노테이션 방식으로 변경

--- src/main/java/core/mvc/asis/RequestMapping.java
+++ src/main/java/core/mvc/asis/RequestMapping.java
@@ -1,6 +1,5 @@
 package core.mvc.asis;
 
-import next.controller.ProfileController;
 import next.controller.UpdateFormUserController;
 import next.controller.UpdateUserController;
 import org.slf4j.Logger;
@@ -16,7 +15,6 @@ public class RequestMapping {
     void initMapping() {
         mappings.put("/users/form", new ForwardController("/user/form.jsp"));
         mappings.put("/users/loginForm", new ForwardController("/user/login.jsp"));
-        mappings.put("/users/profile", new ProfileController());
         mappings.put("/users/updateForm", new UpdateFormUserController());
         mappings.put("/users/update", new UpdateUserController());
 
